84e30dd42685220883f464059214ae9f0ccaf492
Erik Voors <erik.voors@gmail.com>
2018-10-11 21:15:35 +0200

    Also add 70s movies

--- RandomActionMovie/Program.cs
+++ RandomActionMovie/Program.cs
@@ -5,44 +5,66 @@ using System.Text.RegularExpressions;
 
 namespace RandomActionMovie
 {
-    class Program
-    {
+	class Program
+	{
 		static Random random = new Random();
-		const string url80s = "https://en.wikipedia.org/wiki/List_of_action_films_of_the_1980s";
-		const string url90s = "https://en.wikipedia.org/wiki/List_of_action_films_of_the_1990s";
+		const string baseUrl = "https://en.wikipedia.org/wiki/List_of_action_films_of_the_19";
+		const string url70s = baseUrl + "70s";
+		const string url80s = baseUrl + "80s";
+		const string url90s = baseUrl + "90s";
 
 		static void Main(string[] args)
-        {
-			var names = GetNames(url80s);
-			names.AddRange(GetNames(url90s));
-
-			bool next = true;
-			while (next)
+		{
+			var names = new List<HtmlAgilityPack.HtmlNodeCollection>();
+			if (Confirm("Do you want to include 70s movies?", "Including 70s movies", "Not including 70s movies"))
 			{
-				int randomMovieIndex = random.Next(0, names.Count);
-				string name = names.ElementAt(randomMovieIndex)[1].InnerText;
+				names.AddRange(GetNames(url70s));
+			}
+			if (Confirm("Do you want to include 80s movies?", "Including 80s movies", "Not including 80s movies"))
+			{
+				names.AddRange(GetNames(url80s));
+			}
+			if (Confirm("Do you want to include 90s movies?", "Including 90s movies", "Not including 90s movies"))
+			{
+				names.AddRange(GetNames(url90s));
+			}
 
-				while(IsYear(name) || name == "Title")
+			if (names.Count() == 0)
+			{
+				Console.WriteLine("Not showing any movies, press any key to exit");
+				Console.ReadKey();
+			}
+			else
+			{
+				bool next = true;
+				Console.WriteLine("The first movie is:");
+				while (next)
 				{
-					randomMovieIndex = random.Next(0, names.Count);
-					name = names.ElementAt(randomMovieIndex)[1].InnerText;
-				}
+					int randomMovieIndex = random.Next(0, names.Count);
+					string name = names.ElementAt(randomMovieIndex)[1].InnerText;
 
-				string year = GetYear(names, randomMovieIndex);
-				Console.WriteLine($"{name} ({year})\npress Enter for another movie, press any other key to quit.");
-				next = Console.ReadKey().Key == ConsoleKey.Enter;
+					while (IsYear(name) || name == "Title")
+					{
+						randomMovieIndex = random.Next(0, names.Count);
+						name = names.ElementAt(randomMovieIndex)[1].InnerText;
+					}
+
+					string year = GetYear(names, randomMovieIndex);
+					Console.WriteLine($"{name} ({year})");
+					next = Confirm("Do you want another movie?", "The next movie is: ", "Ok, exiting");
+				}
 			}
 		}
 
 		private static string GetYear(IEnumerable<HtmlAgilityPack.HtmlNodeCollection> movies, int movieIndex)
 		{
 			string year = string.Empty;
-			for(int i = movieIndex; i >= 0; i--)
+			for (int i = movieIndex; i >= 0; i--)
 			{
 				string text = movies.ElementAt(i)[1].InnerText;
 				if (IsYear(text))
 				{
-					year = text.Substring(0,4);
+					year = text.Substring(0, 4);
 					break;
 				}
 			}
@@ -62,6 +84,26 @@ namespace RandomActionMovie
 			var table = doc.DocumentNode.SelectNodes("/html/body/div[3]/div[3]/div[4]/div/table[2]/tbody").Descendants(); ;
 			var tableRows = table.Where(d => d.Name == "tr");
 			return tableRows.Select(t => t.ChildNodes).ToList();
+		}
+
+		private static bool Confirm(string question, string positiveAnswer, string negativeAnswer)
+		{
+			bool confirm()
+			{
+				Console.Write(question + " (y/n): ");
+				switch (Console.ReadLine().ToLower())
+				{
+					case "y":
+						return true;
+					case "n":
+						return false;
+					default:
+						return confirm();
+				}
+			}
+			bool confirmed = confirm();
+			Console.WriteLine(confirmed ? positiveAnswer : negativeAnswer);
+			return confirmed;
 		}
 	}
 }
